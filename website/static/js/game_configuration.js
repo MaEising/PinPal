490dac4e6cfe79f775ae7278bb55f8e0046c680a
MaEising <matthis.eysing@googlemail.com>
2023-05-23 00:03:14 +0200

    feat: collapse gametable in game_view

--- website/static/js/game_configuration.js
+++ website/static/js/game_configuration.js
@@ -1,3 +1,5 @@
+let currentRow = null;
+
 function update_quantity(id) {
   console.log("Button id:", id);
   let gameId = id.split("-")[0];
@@ -44,13 +46,33 @@ function update_quantity(id) {
   );
 }
 
+function toggleUserPenalties(tableCellId) {
+  // Check if the clicked row is already revealed
+  if ( currentRow !== null && currentRow[0].id === tableCellId) {
+    // Hide the currently revealed row
+    currentRow.forEach(function (cell) {
+      cell.classList.add("hideRow");
+    });
+    currentRow = null;
+  } else {
+    displayUserPenalties(tableCellId)
+  }
+}
 // Retrieves an id that includes an identifier for all table cells inside a row. On each <td> there is a class set "hideRow" which has to be removed on function call.
 // If a different row is targeted hideRow should be added back again
 // id looks like this {{ player_record.game_id}}-{{ player_record.participant_id }}-tablecell
 function displayUserPenalties(tableCellId) {
-  // Remove "hideRow" class from all table cells with the specified id
   var tableCells = document.querySelectorAll("[id='" + tableCellId + "']");
+  // check if row is currently revealed
+  if (currentRow !== null) {
+    // Hide the currently revealed row
+    currentRow.forEach(function(cell) {
+      cell.classList.add("hideRow")
+    });
+  }
+  // Remove "hideRow" class from all table cells with the specified id
   tableCells.forEach(function (cell) {
     cell.classList.remove("hideRow");
   });
+  currentRow = tableCells;
 }
